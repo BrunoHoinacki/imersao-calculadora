419c05bdae6c139f70d45f973f88edab9d27fa77
Bruno Hoinacki <brunohoinackib@gmail.com>
2022-09-06 18:41:49 -0300

    algumas revisões
    Inacabado

--- JavaScript/calculator.js
+++ JavaScript/calculator.js
@@ -1,15 +1,12 @@
 /* Scripts de JS da calculadora */
+function somarMedia() {
+    var notaDoPrimeiroBimestre = document.getElementById(nota1);
+    var notaDoSegundoBimestre = 7;
+    var notaDoTerceiroBimestre = 4;
+    var notaDoQuartoBimestre = 2;
+    
+    var valorMedia = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4;
+   
+}
 
-var nome = "Bruno";
 
-var notaDoPrimeiroBimestre = 9;
-var notaDoSegundoBimestre = 7;
-var notaDoTerceiroBimestre = 4;
-var notaDoQuartoBimestre = 2;
-
-var NotaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4;
-
-var notaFixada = NotaFinal.toFixed(1);
-
-console.log('Bem vindo ' + nome);
-console.log(notaFixada);
